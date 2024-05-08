ed02716c31c6319eac9c347b6093816dfe461e0f
Leonardo de Oliveira <leeonardo.deo@gmail.com>
2024-05-08 01:41:22 -0300

    Add teleport pad functions

---
+++ unidade_3/QuizGame/Assets/Scripts/TeleportPad.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleportPadBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Player"))
+            return;
+
+        Debug.Log("Collided with a Player");
+        SceneManager.LoadScene("SampleScene");
+    }
+}
