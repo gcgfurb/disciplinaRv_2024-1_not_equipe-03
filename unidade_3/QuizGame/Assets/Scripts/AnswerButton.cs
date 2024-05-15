b43bec6ea3a6fc27926c5e5451adcaac15aa9ada
sbFreitas <sarahb.freitas@gmail.com>
2024-05-15 15:33:31 -0300

    Ajuste cena e botões

--- unidade_3/QuizGame/Assets/Scripts/AnswerButton.cs
+++ unidade_3/QuizGame/Assets/Scripts/AnswerButton.cs
@@ -5,20 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class AnswerButton : MonoBehaviour
 {
+    public int answerIndex;
     // Start is called before the first frame update
-    void Start()
-    {
+    // void Start()
+    // {
         
-    }
+    // }
 
-    // Update is called once per frame
-    void Update()
-    {
+    // // Update is called once per frame
+    // void Update()
+    // {
         
-    }
+    // }
 
     public void OnButtonPress()
     {
-        SceneManager.LoadScene("LobbyScene");
+        //SceneManager.LoadScene("LobbyScene");
+        FindObjectOfType<QuizManager>().SelectAnswer(answerIndex);
     }
 }
